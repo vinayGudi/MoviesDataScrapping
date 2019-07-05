b235a0ed4d9769e5fa3eb2ea82c79149d5ba38ed
vinayGudi <37874414+vinayGudi@users.noreply.github.com>
2019-07-06 02:54:01 +0530

    Add files via upload

--- web app/scarping.py
+++ web app/scarping.py
@@ -1,9 +1,13 @@
 import bs4
-
 from urllib.request import urlopen as uReq
 import pandas as pd 
-
 from bs4 import BeautifulSoup as soup
+import csv
+import sqlalchemy
+engine=sqlalchemy.create_engine("mysql+pymysql://root:8146@localhost:3306/movies")
+
+
+
 starsList=[]
 movieName=[]
 runTimeList=[]
@@ -11,40 +15,21 @@ ratingList=[]
 genreList=[]
 starsList=[]
 se=[]
-    
-
 o=1
 while(True):
     print(o)
-    if o>1700:
+    if o>1500:
         break 
-    
     my_url="https://www.imdb.com/search/title/?genres=horror&start="+str(o)+"&explore=title_type&ref_=adv_nxt"
     o=o+50
-
     uClient=uReq(my_url)
-
-
-
     page_html=uClient.read()
-
-
     uClient.close()
-
-
     page_soup=soup(page_html,"html.parser")
-
-
-    #containers=page_soup.find_all("div",{"class":"Stars:"})
-
-
-    #contain=containers[0]
-    
     s=page_soup.find_all("div",{"class":"lister-item-content"})
     for i in s:
         try:
             movieName.append(i.h3.a.text)
-            
         except:
             movieName.append("NA")
         try:
@@ -57,51 +42,30 @@ while(True):
         except:
             runTimeList.append(-1)
         try:    
-            genre=[j.text for j in i.findAll('span', {'class': 'genre'})]
-            genreList.append(genre[-1])
-            print(genreList)
+            genre=[j.text for j in i.findAll('span', {'class': 'genre'})][0].strip().replace(',','|')
+            ## genreList.append(genre[-1]) 
+            print(genre)
         except:
             genreList.append("NA")
         try:
-            stars=[j.text for j in i.findAll('p', {'class': ''})]
+            stars=[j.text for j in i.findAll('p', {'class': ''})][0].strip().replace('\n','').replace(',','|')
             se.append(stars)
-            for ele in se:
-                for ele2 in ele:
-                    
-                    ele2.strip()
-                    #print(ele2)
-                    StarsList.append(ele2)
-            
                # print(ele)
                #print(stars[-1])
         except:
-            starsList.append("NA")
-    
-    """try: 
-        s=page_soup.find_all("div",{"class":"lister-item mode-advanced"})
-        for i in s:
-            print(i.h3.a.text)
-            print(i.div.div.strong)
-            runtime=[j.text for j in i.findAll('span', {'class': 'runtime'})]
-            print(runtime[0])
-            genre=[j.text for j in i.findAll('span', {'class': 'genre'})]
-            print(genre)
-            stars=[j.text for j in i.findAll('p', {'class': ''})]
-            print(stars)    
-    except:
-        pass
-    """
-   
-#print(starsList)
-d={"MovieName":movieName,"Rating":ratingList,"Genre":genreList,"runtime":runTimeList,"stars":starsList}
-print(len(movieName),len(ratingList),len(genreList),len(runTimeList),len(starsList))
+            starsList.append("NA")   
+
+d={"MovieName":movieName,"Rating":ratingList,"Genre":genre,"Runtime":runTimeList,"Stars":se}
+#print(len(movieName),len(ratingList),len(genre),len(runTimeList),len(starsList))
 df=pd.DataFrame(d)
 print(df.head())
 print(df.shape)
 
-df.to_csv("MoviesData.csv")
+#df.to_csv("MoviesData.csv")
+df.to_sql(name="moviestable",con=engine,index=False,if_exists="replace")
 
-            
+           
+
             
             
             
